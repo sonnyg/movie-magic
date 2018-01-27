acabf149fd180adf6093d01a1624e995188aa3b7
sonnyg <sonny@evagabond.com>
2018-01-27 13:23:18 -0800

    load and play first video

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
-console.log("Yep! I'm here!")
+// create a variable for the playlist
+const playlist = [
+  'videos/blowing-grass.mp4',
+  'videos/casual-chickens.mp4',
+  'videos/city-lights.mp4'
+]
+
+let position = 0;
+
+/*
+  wait for the window to finish loading beore doing anything
+*/
+window.onload = () => {
+  const video = document.getElementById('video')
+
+  video.addEventListener('ended', () => {
+    console.log('video is done')
+  })
+
+  video.src = playlist[position];
+  video.load();
+  video.play();
+}
