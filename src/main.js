f3f52abca2efda582e8ab81411954a3373dbecc3
sonnyg <sonny@evagabond.com>
2018-02-02 10:05:54 -0800

    setting up image buffering and display

--- src/main.js
+++ src/main.js
@@ -23,12 +23,41 @@ function handleWindowLoad() {
   video = document.getElementById('video')
 
   video.addEventListener('ended', handleVideoEnded, false)
+  video.addEventListener('play', handleVideoPlay, false)
 
   loadAndPlayMovie(video, playlist[position])
 }
 
 function handleEffectChange(event) {
-    console.log(`effect selected: ${event.currentTarget.value}`)
+  console.log(`effect selected: ${event.currentTarget.value}`)
+}
+
+function handleVideoPlay() {
+  const video = document.getElementById('video')
+
+  if (video.paused || video.ended) {
+    return
+  }
+
+  const bufferCanvas = document.getElementById('buffer')
+  const displayCanvas = document.getElementById('display')
+  const buffer = bufferCanvas.getContext('2d')
+  const display = displayCanvas.getContext('2d')
+
+  // copy video contents into buffer
+  buffer.drawImage(video, 0, 0, bufferCanvas.width, bufferCanvas.height)
+  const frame = buffer.getImageData(0, 0, bufferCanvas.width, 180) //bufferCanvas.height)
+  const pixels = frame.data.length / 4  // rgba
+
+  console.log(`processing ${pixels} pixels`)
+  for (var i = 0; i < pixels; i++) {
+    // remove all 'red' data
+    frame.data[i * 4 + 0] = 0
+  }
+
+  // copy frame data to display
+  display.putImageData(frame, 0, 0)
+  setTimeout(handleVideoPlay, 0)
 }
 
 function handleVideoEnded() {
